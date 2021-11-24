6c6f06b3abefb0e958a68ad3130c852ea68a5e30
80352126 <sujune@oppo.com>
2021-11-24 10:38:21 +0800

    info:*)update user.jas

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. 3443454 */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource from susu 11-23');
+  res.send('respond with a resource from susu 11-24 10:37');
 });
 
 module.exports = router;
