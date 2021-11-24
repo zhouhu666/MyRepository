25472961a6e70323fce0276a6fbbf20c0b345757
80352126 <sujune@oppo.com>
2021-11-24 15:13:50 +0800

    info:*)11-24下午提交

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource,this is susu01 second test');
+  res.send('respond with a resource,this is susu01 test 2021-11-24 15:13');
 });
 
 module.exports = router;
