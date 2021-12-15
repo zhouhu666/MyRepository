0e386772f30dd1c6fc0a884e158691724c008ed5
June1991 <18681585220@163.com>
2021-12-15 02:28:12 +0000

    更新user.js为susu04

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource, this is susu02 branch');
+  res.send('respond with a resource, this is susu04 branch');
 });
 
 module.exports = router;
