7bba00fe72fb0f623b880ce00e70e6c433a00e54
June1991 <18681585220@163.com>
2021-12-16 02:05:14 +0000

    Update routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource, this is susu04 branch sencond');
+  res.send('respond with a resource, this is susu04 branch three');
 });
 
 module.exports = router;
