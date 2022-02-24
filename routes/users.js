a228bc2607087e3c156106a41a0bf6b8794b2e13
80352126 <sujune@oppo.com>
2022-02-24 11:39:38 +0800

    info:*)更新

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  res.send('respond with a resource, this is susu02 branch 002 jiddids susu02 susu02');
+  res.send('respond with a resource, this is susu02 branch 002 jiddids susu02 susu02 2022-02-24');
   res.send('respond with a resource, this is susu01 branch susu01 jix');
 
 });
