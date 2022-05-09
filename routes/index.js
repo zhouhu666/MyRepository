c10ac37c13e01621b3f7148beda160fb6a64a275
sujune <sujune@oppo.com>
2022-05-09 16:27:42 +0800

    info:*)更新代码susu02

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express susu01 susu01 susu01 susu02' });
+  res.render('index', { title: 'Express susu02 branch index.js' });
 });
 
 module.exports = router;
