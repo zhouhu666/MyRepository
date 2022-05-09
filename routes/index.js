7953a0da3f2e2f12c6bdac2b90b6b2ecfd3e19ef
sujune <sujune@oppo.com>
2022-05-09 16:40:53 +0800

    info:*)更新

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page.susu02 */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express susu02 branch index.js' });
+  res.render('index', { title: 'Express susu02 branch index.js susu02' });
 });
 
 module.exports = router;
