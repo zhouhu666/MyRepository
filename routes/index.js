52f94779ce6ecfecb7e341668e6ea617e94371e9
sujune <sujune@oppo.com>
2022-05-09 16:38:35 +0800

    info:*)revert modify

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page.this is susu04 */
+/* GET home page.*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express susu01 susu01 susu01 susu02' });
 });
