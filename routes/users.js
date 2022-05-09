c055e8ef0a03552f4efde9eb72c8fc253e1fc1e1
sujune <sujune@oppo.com>
2022-05-09 16:50:14 +0800

    info:*)susu04

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. susu04*/
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource, this is susu04 branch four test02');
+  res.send('respond with a resource, this is susu04 branch,do you know');
 });
 
 module.exports = router;
