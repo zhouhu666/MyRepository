616cfe1bb5fd2472d5862cb3d32a96737b3f2daf
sujune <sujune@oppo.com>
2022-06-24 16:28:50 +0800

    info:*) feature susu02

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express susu06' });
+  res.render('index', { title: 'Express this is feature susu02' });
 });
 
 module.exports = router;
