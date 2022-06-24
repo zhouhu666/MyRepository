f9b6dc70852d2fae9dbcd3e3dfbe0328e85478da
sujune <sujune@oppo.com>
2022-06-24 17:00:45 +0800

    info:*)gengx

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express this is feature susu02' });
+  res.render('index', { title: 'Express this is feature susu02,after that susu01' });
 });
 
 router.get('/feature', function(req, res, next) {
