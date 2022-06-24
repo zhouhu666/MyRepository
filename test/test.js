96debde024ce37fe25180a00e688733600d0cad2
sujune <sujune@oppo.com>
2022-06-24 16:36:26 +0800

    info:test更新

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
-  it('has the default page,this is great', function(done) {
+  it('has the default page,this is feature/susu01', function(done) {
     request(app)
       .get('/')
       .expect(/Welcome to Express/, done);
